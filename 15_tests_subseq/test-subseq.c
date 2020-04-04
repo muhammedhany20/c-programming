01a4dd5bc1284ed1c90120d71403f272bef9ad46
Coursera Learner <nobody@nowhere.nul>
2020-04-04 19:13:57 +0000

    Did assignment 15

--- 15_tests_subseq/test-subseq.c
+++ 15_tests_subseq/test-subseq.c
@@ -7,7 +7,7 @@ int main() {
   int array1[] = {1, 2, 4, 1};
   int array2[] = {2, -3, 5, 9,15};
   int array3[] = {-5};
-  int array4[] = {5, 4, 3, 6, 10, 15, -1, 7, 8, 2};
+  int array4[] = {2, 4, 3, 6, 10, 15, -1, 7, 8, 2};
   int array5[] = {3,3,3,4};
   if (maxSeq(NULL, 0)) {
     printf("Failed on NULL\n");
@@ -30,7 +30,7 @@ int main() {
     return EXIT_FAILURE;
   }
   if (maxSeq(array4, 10) != 4) {
-    printf("Failed on 5,4,3,6,10,15,-1,7,8,2\n");
+    printf("Failed on 2,4,3,6,10,15,-1,7,8,2\n");
     return EXIT_FAILURE;
   }
   if (maxSeq(array5, 4) != 2) {
